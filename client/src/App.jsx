2cf22ab2cf32ac4dc559e1013cc9a6e9f12b6e71
Vaibhav Yadav <vaibhavyadav0410@gmail.com>
2025-10-02 19:31:56 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,10 +8,10 @@ import MyBookings from './pages/MyBookings'
 import Footer from './components/Footer'
 import Layout from './pages/owner/Layout'
 import Dashboard from './pages/owner/Dashboard'
+import AddCar from './pages/owner/AddCar'
+import ManageCars from './pages/owner/ManageCars'
+import ManageBookings from './pages/owner/ManageBookings'
 import Login from './components/Login'
-import AddCar from './pages/Owner/AddCar'
-import ManageCars from './pages/Owner/ManageCars'
-import ManageBookings from './pages/Owner/ManageBookings'
 import { Toaster } from 'react-hot-toast'
 import { useAppContext } from './context/AppContext'
 
@@ -22,28 +22,26 @@ const App = () => {
 
   return (
     <>
-    <Toaster />
-      {showLogin && <Login />}
-      
-      {!isOwnerPath && <Navbar />}
+     <Toaster />
+      {showLogin && <Login/>}
 
-      <Routes>
-        <Route path='/' element = {<Home/>}/>
-        <Route path='/car-details/:id' element = {<CarDetails/>}/>
-        <Route path='/cars' element = {<Cars/>}/>
-        <Route path='/my-bookings' element = {<MyBookings/>}/>
-        <Route path='/owner' element={<Layout />}>
-          <Route index element={<Dashboard />}/>
-          <Route path="add-car" element={<AddCar />}/>
-          <Route path="manage-cars" element={<ManageCars />}/>
-          <Route path="manage-bookings" element={<ManageBookings />}/>
-        </Route>
-      </Routes>
+      {!isOwnerPath && <Navbar/>}
 
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/car-details/:id' element={<CarDetails/>}/>
+      <Route path='/cars' element={<Cars/>}/>
+      <Route path='/my-bookings' element={<MyBookings/>}/>
+      <Route path='/owner' element={<Layout />}>
+        <Route index element={<Dashboard />}/>
+        <Route path="add-car" element={<AddCar />}/>
+        <Route path="manage-cars" element={<ManageCars />}/>
+        <Route path="manage-bookings" element={<ManageBookings />}/>
+      </Route>
+    </Routes>
 
-      {!isOwnerPath && <Footer />}
-      
-
+    {!isOwnerPath && <Footer />}
+    
     </>
   )
 }
